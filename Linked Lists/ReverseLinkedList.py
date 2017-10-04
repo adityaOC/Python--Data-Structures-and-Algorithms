79d3a97adfde63da33064b1ce8ee758a8a6377d3
Aditya Gaonkar <adidev70@gmail.com>
2017-10-04 13:34:14 -0500

    reversing linked list

---
+++ Linked Lists/ReverseLinkedList.py
@@ -0,0 +1,58 @@
+"""
+This code reverses the linked list
+
+
+"""
+from SinglyLinkedList import SinglyListNode
+
+def reverseList(SinglyListNode):
+
+   currentNode = SinglyListNode
+   prevNode = None
+   nextNode = None
+
+   while currentNode:
+
+       # Make sure to copy the current nodes next node to a variable next_node
+       # Before overwriting as the previous node for reversal
+       nextNode = currentNode.nextnode
+
+       # Reverse the pointer ot the next_node
+       currentNode.nextnode = prevNode
+
+       # Go one forward in the list
+       prevNode = currentNode
+       currentNode = nextNode
+
+
+   return prevNode
+
+
+# Create a list of 4 nodes
+a = SinglyListNode(1)
+b = SinglyListNode(2)
+c = SinglyListNode(3)
+d = SinglyListNode(4)
+
+# Set up order a,b,c,d with values 1,2,3,4
+a.nextnode = b
+b.nextnode = c
+c.nextnode = d
+
+
+
+
+print(a.nextnode.value)
+print (b.nextnode.value)
+print (c.nextnode.value)
+
+
+
+print("call reverse")
+
+reverseList(a)
+
+print (d.nextnode.value)
+print (c.nextnode.value)
+print (b.nextnode.value)
+
--- Linked Lists/SinglyLinkedList.py
+++ Linked Lists/SinglyLinkedList.py
@@ -1,8 +1,4 @@
 
-"""
-This code checks if there is cycle present in linked list
-
-"""
 
 class SinglyListNode():
 
@@ -13,43 +9,6 @@ class SinglyListNode():
 
 
 
-def checkCycle(SinglyListNode):
-
-    slow = SinglyListNode
-    fast = SinglyListNode
-
-    while fast != None and fast.nextnode != None :
-
-        slow = slow.nextnode
-        fast = fast.nextnode.nextnode
-
-        if slow == fast:
-            return True
-
-    return False
-
-
-
-
-
-
-a = SinglyListNode(1)
-b = SinglyListNode(2)
-c = SinglyListNode(3)
-
-a.nextnode = b
-b.nextnode = c
-c.nextnode = a # Cycle Here!
-
-print(checkCycle(a))
-
-x = SinglyListNode(1)
-y = SinglyListNode(2)
-z = SinglyListNode(3)
-
-x.nextnode = y
-y.nextnode = z
-print(checkCycle(x))
 
 
 
