5cf401c28e5a9652e7bf8437cf07632d523a06b6
Aditya Gaonkar <adidev70@gmail.com>
2017-12-27 11:35:48 -0600

    Quick sort

--- Sorting/QuickSort.py
+++ Sorting/QuickSort.py
@@ -1,12 +1,59 @@
 
 def quickSort(arr):
 
-    left = 0
-    right = len(arr)-1
-
-    partitionPosition = partition(arr,left,right)
-
+   
+    #todo: check for single element in array
+    quickSortHelper(arr,0,len(arr)-1)
 
 
-def partition(arr,left,right):
-    pass
+
+
+
+def quickSortHelper(arr,first,last):
+
+    if first < last:
+
+        partitionPosition = partition(arr, first, last)
+        quickSortHelper(arr,first,partitionPosition-1)
+        quickSortHelper(arr,partitionPosition+1,last)
+
+
+
+
+def partition(arr, first, last):
+    pivotvalue = arr[first]
+
+    leftmark = first + 1
+    rightmark = last
+
+    done = False
+    while not done:
+
+        while leftmark <= rightmark and arr[leftmark] <= pivotvalue:
+            leftmark = leftmark + 1
+
+        while arr[rightmark] >= pivotvalue and rightmark >= leftmark:
+            rightmark = rightmark - 1
+
+        if rightmark < leftmark:
+            done = True
+        else:
+            temp = arr[leftmark]
+            arr[leftmark] = arr[rightmark]
+            arr[rightmark] = temp
+
+    temp = arr[first]
+    arr[first] = arr[rightmark]
+    arr[rightmark] = temp
+
+    return rightmark
+
+
+arr = [2,5,4,6,7,3,1,4,12,11]
+quickSort(arr)
+print(arr)
+
+
+
+
+
